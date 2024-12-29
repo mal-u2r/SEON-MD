c28c6c1ade00b83ef6e9b61d2d2f02066607d888
mal-u2r <malvindarley@gmail.com>
2024-12-29 22:17:20 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Put the session-id here' : process.env.SESSION_ID,
+SESSION_ID: process.env.SESSION_ID === undefined ? 'SEON-MD=yFN2BAZR#-PTNL-ImBCT1tLELjcOGP2Q-KCwILvZscUR_0kYoPIE' : process.env.SESSION_ID,
 GITHUB_USERNAME: process.env.GITHUB_USERNAME === undefined ? 'Put your github username': process.env.GITHUB_USERNAME,
-GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'Put your github auth token': process.env.GITHUB_AUTH_TOKEN
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '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': process.env.GITHUB_AUTH_TOKEN
 };
